c652f6b1d6a5059613146849b3296f0b5e34aac9
Douglas Sjoquist <dwsjoquist@sunetos.com>
2018-09-08 17:57:33 -0400

    Add method

--- IVGFoundation/source/extensions/UIImage+IVGFoundation.swift
+++ IVGFoundation/source/extensions/UIImage+IVGFoundation.swift
@@ -21,5 +21,27 @@ public extension UIImage {
         guard let cgImage = image?.cgImage else { return nil }
         self.init(cgImage: cgImage)
     }
-    
+
+    public func pngCIDInfo(name: String) -> (String,String)? {
+        guard let imageData = UIImagePNGRepresentation(self) else { return nil }
+
+        let imageBase64 = imageData.base64EncodedString(options: .lineLength64Characters)
+        let imageCID = "cid:\(name)"
+        return (imageCID,imageBase64)
+    }
+
+    public func with(size targetSize: CGSize) -> UIImage? {
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        let ratio = (widthRatio > heightRatio) ? heightRatio : widthRatio
+        let newBounds = CGRect(x: 0, y: 0, width: size.width * ratio, height: size.height * ratio)
+
+        UIGraphicsBeginImageContextWithOptions(newBounds.size, false, 1.0)
+        draw(in: newBounds)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+
 }
